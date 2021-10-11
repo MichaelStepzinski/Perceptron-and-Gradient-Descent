bbd83e1bd75df78557b42bd9e7296c843921bc85
DesertPangolin <michaelpstepzinski@gmail.com>
2021-10-11 00:20:48 -0700

    finished gradient descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -4,8 +4,16 @@
 
 import numpy as np
 
-def gradient_descent(function, x_init, learning_rate):
-    print(function)
-    print(x_init)
-    print(learning_rate)
-    pass
+def gradient_descent(grad_f, x_init, learning_rate):
+    
+    features = len(x_init)
+    grad_f_value = x_init
+    iters = 0
+    magnitude = 1
+
+    while magnitude > 0.0001:
+        for x in range(features):
+            grad_f_value[x] = grad_f_value[x] - (learning_rate * grad_f(grad_f_value)[x])
+        magnitude = np.linalg.norm(grad_f_value)
+        iters += 1
+    return grad_f_value
