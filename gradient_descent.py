b9d58f8a0cae7140ce5d8f8558a258e213cab424
DesertPangolin <michaelpstepzinski@gmail.com>
2021-10-15 12:39:44 -0700

    fixed gradient_descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -9,7 +9,7 @@ def gradient_descent(grad_f, x_init, learning_rate):
     # Define number of features, initial gradient of f at x_init values, iters, and magnitude
     num_features = len(x_init)
     grad_f_value = x_init
-    #iters = 0 # used for debugging
+    iters = 0 # used for debugging
     magnitude = 1
 
     # while the magnitude of grad_f at value is greater than (randomly selected) 0.0001
@@ -19,6 +19,6 @@ def gradient_descent(grad_f, x_init, learning_rate):
         for x in range(num_features):
             grad_f_value[x] = grad_f_value[x] - (learning_rate * grad_f(grad_f_value)[x])
         # find magnitude to get the loop to stop
-        magnitude = np.linalg.norm(grad_f_value)
-        #iters += 1
+        magnitude = np.linalg.norm(grad_f(grad_f_value)[x])
+        iters += 1
     return grad_f_value
